b193bf90f87e392960dcd1754395fe5314589a22
goodname427 <1335438725@qq.com>
2024-01-08 21:49:17 +0800

    Update

--- ILoveGraphics/Program.cs
+++ ILoveGraphics/Program.cs
@@ -5,8 +5,9 @@ using MatrixCore;
 //Mesh.Load(@"E:\CGL\Programs\CSharp\ILoveGraphics\ILoveGraphics\models\untitled.obj");
 RenderedScene.RenderedObjects.AddRange(new RenderedObject[]
 {
-    RenderedObject.Cube1,
-    RenderedObject.Cube2,
+    //RenderedObject.Cube1,
+    //RenderedObject.Cube2,
+    RenderedObject.Spot
 });
 RenderedScene.SetConsoleRenderArgs();
 while (true)
