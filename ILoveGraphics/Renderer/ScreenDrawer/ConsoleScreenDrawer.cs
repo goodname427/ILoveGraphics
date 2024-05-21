e207630482b175dd15960e3e574c12e595af5461
goodname427 <106900946+goodname427@users.noreply.github.com>
2024-05-21 18:30:00 +0800

    fix console renderer

--- ILoveGraphics/Renderer/ScreenDrawer/ConsoleScreenDrawer.cs
+++ ILoveGraphics/Renderer/ScreenDrawer/ConsoleScreenDrawer.cs
@@ -15,20 +15,29 @@ namespace ILoveGraphics.Renderer.ScreenDrawer
             var width = frameBuffer.GetLength(0);
             var height = frameBuffer.GetLength(1);
             Console.SetCursorPosition(0, 0);
-            
+
             var output = new StringBuilder();
             var color = ConsoleColor.White;
 
             // 将颜色相同（或者透明）的字符一次性输出
             for (int y = height - 1; y >= 0; y--)
             {
-                for (int x = 0; x < width; x++)
+                for (int x = 0; x < width * 2 - 1; x++)
                 {
-                    var pixelColor = PixelColor.Parse(frameBuffer[x, y]);
+                    PixelColor pixelColor;
+                    if (x % 2 == 0)
+                    {
+                        pixelColor = PixelColor.Parse(frameBuffer[x / 2, y]);
+                    }
+                    else
+                    {
+                        pixelColor = PixelColor.Parse((frameBuffer[x / 2, y] + frameBuffer[(x + 1) / 2, y]) / 2);
+                    }
+
                     if (pixelColor.Gray == 0 || pixelColor.ConsoleColor == color)
                     {
                         output.Append(pixelColor.ConsoleChar);
-                        output.Append(' ');
+                        //output.Append(' ');
                     }
                     else
                     {
